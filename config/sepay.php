287fc871a62267b278370d45818a36f97e44d9d4
Henry <ppkhanh190404@gmail.com>
2025-10-21 12:48:27 +0700

    up

--- config/sepay.php
+++ config/sepay.php
@@ -4,9 +4,11 @@ return [
     'merchant_code' => env('SEPAY_MERCHANT_CODE'),
     'api_key' => env('SEPAY_API_KEY'),
     'checksum_key' => env('SEPAY_CHECKSUM_KEY'),
-    'payment_url' => env('SEPAY_PAYMENT_URL', 'https://pay.sepay.vn/payment'),
-    'return_url' => env('SEPAY_RETURN_URL', env('APP_URL') . '/payments/sepay/return'),
+    'payment_url' => env('SEPAY_PAYMENT_URL'),
+    'return_url' => env('SEPAY_RETURN_URL'),
     'log_channel' => env('SEPAY_LOG_CHANNEL', 'stack'),
     'webhook_token' => env('SEPAY_WEBHOOK_TOKEN'),
     'pattern' => env('SEPAY_MATCH_PATTERN', 'SE'),
+    'account' => env('SEPAY_ACCOUNT'),
+    'bank' => env('SEPAY_BANK'),
 ];
